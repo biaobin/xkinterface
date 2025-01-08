5f357fe5aff5507643e35b3c56acb368c27b0b89
Xiangkun Li <lixiangk@wgs55.zeuthen.desy.de>
2025-01-08 13:53:51 +0100

    'Regular update'

---
+++ tutorials/create_jobs.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from interface import *
+
+# Environment to load
+activate = '. /afs/ifh.de/group/pitz/data/lixiangk/work/apps/python3/3.9.18/bin/activate'
+#activate = 'module add python/3.9'
+
+command = 'python'
+script = 'TripletTuning.py'
+
+job = CondorJob(command = command)
+
+jobname = f'temp.sh'
+args = [1]
+submit = False
+
+job.create(jobName = jobname, 
+           inputName = script, 
+           submit = submit, 
+           args = args, 
+           activate = activate)
---
+++ tutorials/temp.sh
@@ -0,0 +1,3 @@
+cd .
+. /afs/ifh.de/group/pitz/data/lixiangk/work/apps/python3/3.9.18/bin/activate
+python TripletTuning.py 1  2>&1 | tee TripletTuning.log
